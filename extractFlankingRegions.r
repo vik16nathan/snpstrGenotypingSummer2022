89140e3de380be6b367dfc5d0ccc9f2daad4da9a
Vikram Nathan <vikram.nathan@mail.mcgill.ca>
2022-06-21 11:52:50 +0000

    Replace extractFlankingRegions.r

--- extractFlankingRegions.r
+++ extractFlankingRegions.r
@@ -4,7 +4,6 @@ args = commandArgs(trailingOnly=TRUE)
 if (length(args)!=2) {
   stop("Must supply primer and sample!", call.=FALSE)
 } 
-
 primer <- args[1]
 sample <- args[2]
 
@@ -23,7 +22,7 @@ fasta_sequences <- readLines(paste("./FastaInputs/",
 all_left_flanks <- c()
 all_right_flanks <- c()
 all_motifs <- c()
-output_colnames <- c("5'Flank", "3'Flank",	"Motif")
+output_colnames <- c("5'Flank", "3'Flank","Motif")
 for(row in 1:nrow(perf_input))
 {
     
@@ -37,8 +36,8 @@ for(row in 1:nrow(perf_input))
     #many samples will simply be discarded, preventing genotyping for certain primers
 
     #note: investigate ideal number later
-    left_flank <- c(substr(fasta_sequences[2*row],start_point-10, start_point))
-    right_flank <- c(substr(fasta_sequences[2*row],end_point+1, end_point+11))
+    left_flank <- c(substr(fasta_sequences[2*row],start_point-9, start_point))
+    right_flank <- c(substr(fasta_sequences[2*row],end_point+1, end_point+10))
 
     #prepare output .config file for each primer
     motif <- c(noquote(format(perf_input[row,8])))
@@ -53,3 +52,6 @@ output_df <- data.frame(all_left_flanks, all_right_flanks, all_motifs)
 colnames(output_df) <- output_colnames
 write.table(output_df, paste("./separatedFlanks/",primer_sample_prefix_string,"_flanks.tsv",sep=""),
             row.names=FALSE,quote=FALSE,sep="\t")
+
+
+
