007b02a3f939019c79cfccc4f61eb96ee20982be
Vikram Nathan <vikram.nathan@mail.mcgill.ca>
2022-06-21 11:43:22 +0000

    issue - this file throws out all genotypes that have an insertion on the first b.p.

--- getSTRaitRazorGenotypes.R
+++ getSTRaitRazorGenotypes.R
@@ -40,58 +40,54 @@ for(primer in list_of_primers_to_include)
                     repeat_motif <- substr(strait_razor_output[1,1],1,4)
                     #make sure to count reads only for matches that don't correspond to a flanking region insertion
                     non_inserted_alleles <- which(substr(repeat_motif,1,1) == substr(strait_razor_output[,4],1,1))
-                    total_num_reads <- sum(strait_razor_output[non_inserted_alleles,5],
-                                            strait_razor_output[non_inserted_alleles,6])
-                    #IMPORTANT: reject all primer/sample combinations for which there are <10 reads!!
-                    if(total_num_reads >=10)
+          
+                    
+                    #Create a vector to represent "significant" alleles, with threshold as a user input
+                    significant_alleles <- c()
+                    all_primers <- c(all_primers, primer)
+                    all_samples <- c(all_samples, sample)
+                    for(i in c(1:nrow(strait_razor_output)))
                     {
-                        
-                        #Create a vector to represent "significant" alleles, with threshold as a user input
-                        significant_alleles <- c()
-                        all_primers <- c(all_primers, primer)
-                        all_samples <- c(all_samples, sample)
-                        for(i in c(1:nrow(strait_razor_output)))
+                        genotype <- format(strait_razor_output[i,4])
+                        repeat_motif <- substr(strait_razor_output[i,1],1,4)
+                        num_reads_per_allele <- strait_razor_output[i,5]+strait_razor_output[i,6]
+                        if(num_reads_per_allele/total_num_reads > threshold && 
+                                substr(genotype,1,1) == substr(repeat_motif,1,1))
                         {
-                            genotype <- format(strait_razor_output[i,4])
-                            repeat_motif <- substr(strait_razor_output[i,1],1,4)
-                            num_reads_per_allele <- strait_razor_output[i,5]+strait_razor_output[i,6]
-                            if(num_reads_per_allele/total_num_reads > threshold && 
-                                    substr(genotype,1,1) == substr(repeat_motif,1,1))
-                            {
-                                significant_alleles <- c(significant_alleles, genotype)
-                            }
-                        }
-
-                        #IMPORTANT - account for insertions in the flanking region (1E error)
-                        #Add significant allele only if its first character is the same as the repeat motif
-
-                        #Note: there is still an edge case that isn't addressed by this - what if there's a point
-                        #mutation in the first character of the STR? Then, we won't identify it as a significant
-                        #allele, when it is very much significant
-
-                        #note - significant alleles are always in order of frequency
-                        if(length(significant_alleles)>0)
-                        {
-                            significant_alleles <- rev(significant_alleles[order(nchar(significant_alleles), 
-                                                                        significant_alleles)])
-                            all_allele_1 <- c(all_allele_1,significant_alleles[1])
-                            #Case 1 - one significant allele - homozygous
-                            if(length(significant_alleles) == 1)
-                            {
-                                all_allele_2 <- c(all_allele_2,significant_alleles[1])
-                            }
-                            #Case 2 - two significant alleles  - heterozygous
-                            #Case 3 - more than two significant alleles - pick the two most frequent, call it a heterozygote
-                            #alleles are already in descending order of frequency based on the ordering of output rows from STRait razor
-                            #note that this doesn't happen often - only 1 time out of ~200 configurations
-                            else
-                            {
-                                all_allele_2 <- c(all_allele_2,significant_alleles[2])
-                            }
-                            all_primers <- c(all_primers, primer)
-                            all_samples <- c(all_samples, sample)
+                            significant_alleles <- c(significant_alleles, genotype)
                         }
                     }
+
+                    #IMPORTANT - account for insertions in the flanking region (1E error)
+                    #Add significant allele only if its first character is the same as the repeat motif
+
+                    #Note: there is still an edge case that isn't addressed by this - what if there's a point
+                    #mutation in the first character of the STR? Then, we won't identify it as a significant
+                    #allele, when it is very much significant
+
+                    #note - significant alleles are always in order of frequency
+                    if(length(significant_alleles)>0)
+                    {
+                        significant_alleles <- rev(significant_alleles[order(nchar(significant_alleles), 
+                                                                    significant_alleles)])
+                        all_allele_1 <- c(all_allele_1,significant_alleles[1])
+                        #Case 1 - one significant allele - homozygous
+                        if(length(significant_alleles) == 1)
+                        {
+                            all_allele_2 <- c(all_allele_2,significant_alleles[1])
+                        }
+                        #Case 2 - two significant alleles  - heterozygous
+                        #Case 3 - more than two significant alleles - pick the two most frequent, call it a heterozygote
+                        #alleles are already in descending order of frequency based on the ordering of output rows from STRait razor
+                        #note that this doesn't happen often - only 1 time out of ~200 configurations
+                        else
+                        {
+                            all_allele_2 <- c(all_allele_2,significant_alleles[2])
+                        }
+                        all_primers <- c(all_primers, primer)
+                        all_samples <- c(all_samples, sample)
+                    }
+                
                    
                 }
     }
