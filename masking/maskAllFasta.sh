8421e2338e24f86e7a902d68921abf2239a75061
Vikram Nathan <vikram.nathan@mail.mcgill.ca>
2022-06-29 08:04:43 +0000

    Replace maskAllFasta.sh to include other post-processing steps required to find SNPs

--- masking/maskAllFasta.sh
+++ masking/maskAllFasta.sh
@@ -11,14 +11,24 @@ do
 
         filePrefix="P-pyrhulla_$sample.sorted.duplicates_Primer$primer""_allele$allele"
         #use masking tool on each DNA sequencing reads for each allele from each primer/sample pair
-        bedtools maskfasta -fi "./FastaInputs/$filePrefix.fasta" -bed "bedForMasking/P-pyrhulla_$sample""_Primer$primer""_allele_$allele.bed" -fo "./maskedFasta/$filePrefix""_masked.fasta"
+        bedtools maskfasta -fi "./FastaInputs/$filePrefix.fasta" -bed "bedForMasking/P-pyrhulla_$sample""_Primer$primer""_allele_$allele.bed" -fo "./maskedFasta/$filePrefix""_masked_R1.fasta"
+        
+        #ensure that reads aren't split across multiple lines
+        forwardFileName="./maskedFasta/$filePrefix""_masked_combined_R1.fasta"
+        (seqtk seq -a "./maskedFasta/$filePrefix""_masked_R1.fasta") > $forwardFileName
+
+        #create a "fake" reverse fasta by reversing all lines in the original .fasta
+        Rscript createReverseFasta.r $prefix $forwardFileName
+            
+        (seqtk seq -a "./maskedFasta/$filePrefix""_masked_R2.fasta") > "./maskedFasta/$filePrefix""_masked_combined_R2.fasta"
 
         #create .fai file for new fastas
-        samtools faidx "./maskedFasta/$filePrefix""_masked.fasta" -o "./maskedFasta/$filePrefix""_masked.fasta.fai"
+        samtools faidx "./maskedFasta/$filePrefix""_masked_combined_R1.fasta"
+        samtools faidx "./maskedFasta/$filePrefix""_masked_combined_R2.fasta"
 
         #create .dict file
-        gatk CreateSequenceDictionary -R "./maskedFasta/$filePrefix""_masked.fasta"
-
+        gatk CreateSequenceDictionary -R "./maskedFasta/$filePrefix""_masked_combined_R1.fasta"
+        gatk CreateSequenceDictionary -R "./maskedFasta/$filePrefix""_masked_combined_R2.fasta"
 
         done
     done
