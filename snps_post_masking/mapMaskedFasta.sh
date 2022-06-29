645e833a6dee53483d450a1aac5566e4d44ac2a3
Vikram Nathan <vikram.nathan@mail.mcgill.ca>
2022-06-29 16:01:57 +0000

    Replace mapMaskedFasta.sh

--- snps_post_masking/mapMaskedFasta.sh
+++ snps_post_masking/mapMaskedFasta.sh
@@ -1,18 +1,20 @@
 #!/bin/bash -i
 
+#rename to mapMaskedFastq.sh
+
 #listOfSamples=("1232" "1393" "1791" "2006092" "2006174" "217" "2520669" "2599208" "34896" "34966")
 for sample in 1232
 do
-    #for primer in {1..30}
-    for primer in {1..15}
+    for primer in {1..30}
     do
         for allele in 1 2
         do
 
-        fastaPrefix="P-pyrhulla_$sample.sorted.duplicates_Primer$primer""_allele$allele""_masked_combined"
-        bwa index "./maskedFasta/$fastaPrefix.fasta"
-        SampleID="$primer""_$sample""_$allele"
-        bwa mem -R "@RG\tID:${SampleID}\tLB:$sample\tPL:ILLUMINA\tPM:HISEQ\tSM:${SampleID}" Pyrhulla_pyrhulla_PrimerRef.fasta "./maskedFasta/$fastaPrefix""_R1.fasta" "./maskedFasta/$fastaPrefix""_R2.fasta" > "./maskedSeparatedSams/$fastaPrefix.mapped.sam"
+        fastqPrefix="P-pyrhulla_$sample""_Primer$primer""_allele$allele""_masked_combined"
+        bwa index "./maskedFastq/$fastqPrefix""_R1.fastq"
+        bwa index "./maskedFastq/$fastqPrefix""_R2.fastq"
+        SampleID="$sample""_$allele"
+        bwa mem -R "@RG\tID:${SampleID}\tLB:$sample\tPL:ILLUMINA\tPM:HISEQ\tSM:${SampleID}" ./onePrimerFastas/Pyrhulla_pyrhulla_Primer$primer.fasta "./maskedFastq/$fastqPrefix""_R1.fastq" "./maskedFastq/$fastqPrefix""_R2.fastq" > "./maskedSeparatedSams/$fastqPrefix.mapped.sam"
 
         done
     done
