033ca918d96f416158a85b131c0c4fb4885f9fbb
Vikram Nathan <vikram.nathan@mail.mcgill.ca>
2022-06-29 16:03:40 +0000

    Replace CallVariantsMaskedSeparated.sh

--- snps_post_masking/CallVariantsMaskedSeparated.sh
+++ snps_post_masking/CallVariantsMaskedSeparated.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -i
 
-#make dictionary for reference fasta
-gatk CreateSequenceDictionary -R Pyrhulla_pyrhulla_PrimerRef.fasta
 
 listOfSamples=("1232" "1393" "1791" "2006092" "2006174" "217" "2520669" "2599208" "34896" "34966")
 #for sample in "${listOfSamples[@]}"
@@ -11,14 +9,26 @@ do
     do
         for allele in 1 2
         do
-        prefix="P-pyrhulla_$sample.sorted.duplicates_Primer$primer""_allele$allele""_masked_combined"
-        samtools index "./maskedSeparatedSams/$prefix.sorted.duplicates.bam"
+        echo "*******************************************************************"
+        echo "Primer: $primer , sample: $sample , allele: $allele"
+        echo "*******************************************************************"
+        prefix="P-pyrhulla_$sample""_Primer$primer""_allele$allele""_masked_combined"
+
+        inputBam="./maskedSeparatedSams/$prefix.sorted.duplicates.bam"
+        samtools index $inputBam
+        numReads=$(samtools view -c $inputBam)
+        
+        echo "*******************************************************************"
+        echo "Total number of reads: $numReads"
+        echo "*******************************************************************"
+
         gatk --java-options "-Xmx4g" HaplotypeCaller \
-        -R Pyrhulla_pyrhulla_PrimerRef.fasta \
-        -I  "./maskedSeparatedSams/$prefix.sorted.duplicates.bam" \
+        -R "./onePrimerFastas/Pyrhulla_pyrhulla_Primer$primer.fasta" \
+        -I  $inputBam \
         -O "./maskedSeparatedGVCFs/$prefix.g.vcf.gz" \
+        --disable-read-filter "NotDuplicateReadFilter" \
+        --disable-read-filter "WellformedReadFilter" \
         -ERC GVCF
         done
     done
 done
-
