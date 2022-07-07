5089dc68b4da9fb99694d435bbc4223ae07c9d42
Vikram Nathan <vikram.nathan@mail.mcgill.ca>
2022-07-07 14:17:06 +0000

    updated to have "true" R1 and R2 reads before masking, and to reverse the once-reversed R2 reads to be in the 3'-5' direction

--- masking/maskAllFasta.sh
+++ masking/maskAllFasta.sh
@@ -9,26 +9,28 @@ do
         for allele in 1 2
         do
 
-        filePrefix="P-pyrhulla_$sample.sorted.duplicates_Primer$primer""_allele$allele"
+        outputFilePrefix="P-pyrhulla_$sample""_Primer$primer""_allele$allele"
         #use masking tool on each DNA sequencing reads for each allele from each primer/sample pair
-        bedtools maskfasta -fi "./FastaInputs/$filePrefix.fasta" -bed "bedForMasking/P-pyrhulla_$sample""_Primer$primer""_allele_$allele.bed" -fo "./maskedFasta/$filePrefix""_masked_R1.fasta"
-        
-        #ensure that reads aren't split across multiple lines
-        forwardFileName="./maskedFasta/$filePrefix""_masked_combined_R1.fasta"
-        (seqtk seq -a "./maskedFasta/$filePrefix""_masked_R1.fasta") > $forwardFileName
+        bedtools maskfasta -fi "./FastaInputs/${outputFilePrefix}_R1.fasta" -bed "bedForMasking/P-pyrhulla_$sample""_Primer$primer""_allele_$allele.bed" -fo "./maskedFasta/$outputFilePrefix""_masked_R1.fasta"
+        bedtools maskfasta -fi "./FastaInputs/${outputFilePrefix}_R2_reversed.fasta" -bed "bedForMasking/P-pyrhulla_$sample""_Primer$primer""_allele_$allele.bed" -fo "./maskedFasta/$outputFilePrefix""_masked_R2_reversed.fasta"
 
-        #create a "fake" reverse fasta by reversing all lines in the original .fasta
-        Rscript createReverseFasta.r $prefix $forwardFileName
-            
-        (seqtk seq -a "./maskedFasta/$filePrefix""_masked_R2.fasta") > "./maskedFasta/$filePrefix""_masked_combined_R2.fasta"
+        #ensure that reads aren't split across multiple lines
+        forwardFileName="./maskedFasta/$outputFilePrefix""_masked_combined_R1.fasta"
+        r2reverseFileName="./maskedFasta/$outputFilePrefix""_masked_combined_R2_reversed.fasta"
+        prefix="./maskedFasta/$outputFilePrefix""_masked_combined"
+        (seqtk seq -a "./maskedFasta/$outputFilePrefix""_masked_R1.fasta") > $forwardFileName
+        (seqtk seq -a "./maskedFasta/$outputFilePrefix""_masked_R2_reversed.fasta") > $r2reverseFileName
+
+        #Reverse the R2 fasta lines to be back in their original 3'-5' orientation
+        Rscript createReverseFasta.r $prefix $r2reverseFileName
 
         #create .fai file for new fastas
-        samtools faidx "./maskedFasta/$filePrefix""_masked_combined_R1.fasta"
-        samtools faidx "./maskedFasta/$filePrefix""_masked_combined_R2.fasta"
+        samtools faidx "./maskedFasta/$outputFilePrefix""_masked_combined_R1.fasta"
+        samtools faidx "./maskedFasta/$outputFilePrefix""_masked_combined_R2.fasta"
 
         #create .dict file
-        gatk CreateSequenceDictionary -R "./maskedFasta/$filePrefix""_masked_combined_R1.fasta"
-        gatk CreateSequenceDictionary -R "./maskedFasta/$filePrefix""_masked_combined_R2.fasta"
+        gatk CreateSequenceDictionary -R "./maskedFasta/$outputFilePrefix""_masked_combined_R1.fasta"
+        gatk CreateSequenceDictionary -R "./maskedFasta/$outputFilePrefix""_masked_combined_R2.fasta"
 
         done
     done
