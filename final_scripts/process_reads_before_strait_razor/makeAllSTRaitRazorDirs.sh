9b6e53cd16408ef3573d643c426565a699eb762a
Vikram Nathan <vikram.nathan@mail.mcgill.ca>
2022-07-28 13:04:54 +0000

    Upload New File

---
+++ final_scripts/process_reads_before_strait_razor/makeAllSTRaitRazorDirs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir separatedBams
+mkdir separatedFilteredBams
+mkdir FastaInputs
+mkdir FastqInputs
+mkdir PERFallReads
+mkdir separatedFlanks
+mkdir config
+mkdir ./config/beforeSlidingWindow
+mkdir possibleIncorrectFlanks
+mkdir actualIncorrectFlanksWithRef
+mkdir correctedFlanks
+mkdir results
+
