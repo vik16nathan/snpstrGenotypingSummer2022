4a4ba71ec0eca4f2a6e5cd6cdfa2b071d3578e27
Vikram Nathan <vikram.nathan@mail.mcgill.ca>
2022-08-03 11:01:45 +0000

    Update fixAllConfig.sh

--- final_scripts/config_and_strait_razor/fixAllConfig.sh
+++ final_scripts/config_and_strait_razor/fixAllConfig.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+sampleListFile=$1
 for primer in {1..30}
 do
 
     Rscript findIncorrectFlanks.r $primer
-    Rscript findReferencesForIncorrectFlanks.r $primer
+    Rscript findReferencesForIncorrectFlanks.r $primer $sampleListFile
     Rscript findTrueFlanks.r $primer
     Rscript fixConfigFile.r $primer
 
