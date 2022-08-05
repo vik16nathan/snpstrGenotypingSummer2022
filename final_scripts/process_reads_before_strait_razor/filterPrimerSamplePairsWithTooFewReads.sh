94c321978547c5e3871283a7a03e1c5558208f1f
Vikram Nathan <vikram.nathan@mail.mcgill.ca>
2022-08-05 11:57:01 +0000

    Update filterPrimerSamplePairsWithTooFewReads.sh

--- final_scripts/process_reads_before_strait_razor/filterPrimerSamplePairsWithTooFewReads.sh
+++ final_scripts/process_reads_before_strait_razor/filterPrimerSamplePairsWithTooFewReads.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -i
 
 sampleListFile=$1
+threshold=$2
 mapfile -t listOfSamples < $sampleListFile
 for sample in "${listOfSamples[@]}"
 do
     for primer in {1..30}
     do
-        if [ $(samtools view -c -F 260 ./separatedBams/${sample}.sorted.duplicates_Primer${primer}.bam) -gt 10 ]
+        if [ $(samtools view -c -F 260 ./separatedBams/${sample}.sorted.duplicates_Primer${primer}.bam) -gt ${threshold} ]
         then
             cp ./separatedBams/${sample}.sorted.duplicates_Primer${primer}.bam ./separatedFilteredBams/
         fi
