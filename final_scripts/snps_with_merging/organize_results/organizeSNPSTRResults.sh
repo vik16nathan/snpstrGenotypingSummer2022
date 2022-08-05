d705952c3b6656ae81c92b9f405b14511f1a168b
Vikram Nathan <vikram.nathan@mail.mcgill.ca>
2022-08-05 10:18:19 +0000

    Replace organizeSNPSTRResults.sh

--- final_scripts/snps_with_merging/organize_results/organizeSNPSTRResults.sh
+++ final_scripts/snps_with_merging/organize_results/organizeSNPSTRResults.sh
@@ -4,15 +4,24 @@
 #Before running this, edit this file with the line number where the table column names are 
 #in the .vcf file
 
-#IMPORTANT!! Replace 74 with the actual line number where table starts
+#For makeFinalOutputTable.r:
+#We also need to store the primer, starting position, ending position, and repeat motif 
+#of the STR within the reference genome in .bed file format (without any column names, and separated by a space/tab)
+#EDIT THESE POSITIONS BY HAND!!
+#... this could also make the original, non-fixed .config files a bit less erroneous, since we have to do this by hand anyways
+
 sampleListFile=$1
-tableStart=74
+tableStart=$(grep -n "#CHROM" MergedVariantsFilteredAnnotated.vcf | cut -d : -f 1)
 tail -n +$tableStart MergedVariantsFilteredAnnotated.vcf > MergedVariantsFilteredAnnotatedTableOnly.vcf
 
 ./VariantsToTable2Merged.sh
 
-Rscript processFinalVariantTable.r MergedVariantsFilteredAnnotated.table MergedVariantsFilteredAnnotatedTableOnly.vcf  $sampleListFile
+Rscript processFinalVariantTable.r MergedVariantsFilteredAnnotated.table MergedVariantsFilteredAnnotatedTableOnly.vcf
 
 mkdir finalExcelOutputs
-#Rscript makeFinalOutputTable.r $sampleListFile
-Rscript makeCondensedFinalOutputTable.r $sampleListFile
+
+Rscript findPerfBedSTRpositions.r
+#IMPORTANT - BY HAND - make sure the _fixed.bed file has the proper starting and ending positions of the STRs
+#PERF isn't always perfect!
+cp PrimerRef_perf_not_fixed.bed PrimerRef_perf_fixed.bed
+Rscript makeFinalOutputTable.r $sampleListFile
