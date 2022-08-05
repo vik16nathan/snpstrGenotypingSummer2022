d758fd9577b2402d586fe4b58027bf9ef7bad3e7
Vikram Nathan <vikram.nathan@mail.mcgill.ca>
2022-08-05 08:31:10 +0000

    Update makeConfigAndRunSTRaitRazor.sh

--- final_scripts/config_and_strait_razor/makeConfigAndRunSTRaitRazor.sh
+++ final_scripts/config_and_strait_razor/makeConfigAndRunSTRaitRazor.sh
@@ -4,12 +4,13 @@
 #Use PERF and have each flank be 10 b.p. long
 ref=$1
 sampleListFile=$2
+numberOfExtraFlanks=$3 
 cp $ref PrimerRef.fasta
 PERF -i PrimerRef.fasta #output filename: PrimerRef_perf.tsv
 Rscript PERF_tsv_to_STRaitRazor_config.r 10
 
 #Use more complicated process to fix .config files
-./makeConfigFromAllReadsFasta.sh $sampleListFile
+./makeConfigFromAllReadsFasta.sh $sampleListFile $numberOfExtraFlanks
 #need extractFlankingRegions.r and filterFlankingRegions.r
 
 ./fixAllConfig.sh $sampleListFile
